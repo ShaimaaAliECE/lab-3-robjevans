a2e5fd7f169267d56fcfbed94a672201ff52a8ab
Robert <revans48@uwo.ca>
2024-02-02 20:13:52 -0500

    changes

---
+++ Lab3App/Axe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3App
+{
+    class Axe : Tool
+    {
+        public Axe(string a) : base(a)
+        {
+
+        }
+        public override void DoAction()
+        {
+            Console.WriteLine("Axe is used");
+        }
+    }
+}
---
+++ Lab3App/Diamond.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3App
+{
+    class Diamond : Treasure
+    {
+        public Diamond(string dia, int score) : base(dia, score)
+        {
+
+        }
+
+
+        public override void Display()
+        {
+            Console.WriteLine(this.Description + " is displayed");
+        }
+    }
+}
